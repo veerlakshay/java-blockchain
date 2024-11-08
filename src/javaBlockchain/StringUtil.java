eeb67277a34c1d3bea8bf512b3d3ad251c872230
Lakshay <lakshay.rv@gmail.com>
2024-11-08 18:39:19 -0500

    Transaction updated

--- src/javaBlockchain/StringUtil.java
+++ src/javaBlockchain/StringUtil.java
@@ -1,6 +1,8 @@
 package javaBlockchain;
 
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
+
 public class StringUtil {
     //Apply  Sha256 to a string and returns the result
     public static String applySha256(String input){
@@ -20,5 +22,39 @@ public class StringUtil {
         catch(Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    public static byte[] applyECDSASig(PrivateKey privateKey , String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+
+        try{
+            dsa = Signature.getInstance("ECDSA" , "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            byte[] realSig = dsa.sign();
+            output = realSig;
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static boolean verifyECDSASig (PublicKey publicKey , String data , byte[] signature) {
+        try{
+            Signature ecdsaVerify = Signature.getInstance("ECDSA" , "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(Key key){
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }

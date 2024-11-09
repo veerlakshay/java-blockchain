e80104c56686f8bede456ac9ffaf7ff4e1a6e025
Lakshay <lakshay.rv@gmail.com>
2024-11-08 20:43:26 -0500

    TransactionInput updated

--- src/javaBlockchain/TransactionInput.java
+++ src/javaBlockchain/TransactionInput.java
@@ -1,4 +1,13 @@
 package javaBlockchain;
 
 public class TransactionInput {
+
+    //reference to transactionOutputs -> transactionId
+    public String transactionOutputId;
+    //contains the unspent transaction output
+    public TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId){
+        this.transactionOutputId = transactionOutputId;
+    }
 }

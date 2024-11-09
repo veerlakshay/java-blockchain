f7b7b31bb109101b83b05c465bc0c2363360f5af
Lakshay <lakshay.rv@gmail.com>
2024-11-08 20:48:48 -0500

    TransactionOutput updated

--- src/javaBlockchain/TransactionOutput.java
+++ src/javaBlockchain/TransactionOutput.java
@@ -1,4 +1,29 @@
 package javaBlockchain;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
+    public String id;
+
+    //new owner of the coins
+    public PublicKey reciepient;
+
+    //amout of coin they own
+    public float value;
+
+    //the id of the transaction this output was created in
+    public String parentTransactionId;
+
+    public TransactionOutput(PublicKey reciepient , float value , String parentTransactionId){
+        this.reciepient = reciepient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(reciepient) + Float.toString(value) + parentTransactionId);
+
+    }
+
+    //check if coin belongs to you
+    public boolean isMine(PublicKey publicKey){
+        return (publicKey == reciepient);
+    }
 }
